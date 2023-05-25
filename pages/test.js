bfcc277cfc0bf226d7690963695d84696a1cf5ec
rezasetyawan <setyawanreza960@gmail.com>
2023-05-25 09:43:32 +0700

    idk man

--- pages/test.js
+++ pages/test.js
@@ -1,15 +1,15 @@
-// import NavbarPR from "@/components/public-relation/Navbar";
-// import PublicRelationHome from "@/components/public-relation/Home";
-// import { useState } from "react";
-// import CompanyList from "@/components/CompanyList";
-// import Link from "next/link";
-// import AddCompanyAccountForm from "@/components/public-relation/AddCompanyAccountForm";
+import NavbarPR from "@/components/public-relation/Navbar";
+import PublicRelationHome from "@/components/public-relation/Home";
+import { useState } from "react";
+import CompanyList from "@/components/CompanyList";
+import Link from "next/link";
+import AddCompanyAccountForm from "@/components/public-relation/AddCompanyAccountForm";
 
-// export default function TestPage() {
-//   const [sidebar, setSideBar] = useState(false);
-//   return (
-//     <>
-//       <AddCompanyAccountForm></AddCompanyAccountForm>
-//     </>
-//   );
-// }
+export default function TestPage() {
+  const [sidebar, setSideBar] = useState(false);
+  return (
+    <>
+      <AddCompanyAccountForm></AddCompanyAccountForm>
+    </>
+  );
+}
