12ca3cf62e011751fccb26f933a41625906ce693
rezasetyawan <setyawanreza960@gmail.com>
2023-05-25 10:26:02 +0700

    change dynamic routes from static to server

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import NotFoundVector from "../public/logo/404-vector.svg";
 export default function Error404() {
   return (
-    <article className="w-full h-full flex justify-center items-center">
+    <article className="w-full h-screen flex justify-center items-center">
       <div>
         <NotFoundVector />
         <h2 className="font-sans font-bold text-lg text-center">Page Not Found</h2>
